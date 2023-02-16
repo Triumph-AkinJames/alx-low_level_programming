75e12e3cd3c51bffabc5698c143113b684d970cc
Triumph-Akin-James <triumphakinjames@gmail.com>
2023-02-16 17:16:32 +0100

    Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle") ;
-	return (0) ;
+	return(0) ;
 }
