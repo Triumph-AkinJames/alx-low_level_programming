be6c2097e620c03fd1ef124de86e4756c5e36c9e
Triumph-Akin-James <triumphakinjames@gmail.com>
2023-02-22 01:13:30 +0000

    Changes

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * Print_sign - Prints the sign of a number
+ * print_sign - Prints the sign of a number
  * @n: The number to be checked
  * Return: 1 for positive numbers, -1 for negative num or zero for anything
  */
