41d2c53f249cfcf454e43bf6213f44e67e04a992
Triumph-Akin-James <triumphakinjames@gmail.com>
2023-03-01 17:16:02 +0000

    Changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -20,7 +20,7 @@ char *_strncpy(char *dest, char *src, int n)
 	dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
-	dest[index] = '\10';
+	dest[index] = '\0';
 
 	return (dest);
 }
