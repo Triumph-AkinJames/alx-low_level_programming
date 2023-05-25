15738a15bd0861b4cb2a136f4e1f9e081629f9f2
Triumph Akin-James <triumphakinjames@gmail.com>
2023-05-25 03:34:23 +0100

    Fibonnaci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 52 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
