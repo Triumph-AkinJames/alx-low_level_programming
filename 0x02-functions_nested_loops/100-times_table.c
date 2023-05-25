be3da8faa0a3db403feaae098c9e1cdf589b6b65
Triumph Akin-James <triumphakinjames@gmail.com>
2023-05-25 03:17:33 +0100

    Print times tables

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_times_tables - prints the times table of the input
+ * starting with 0
+ * @n: The value of the times table to be printed
+ */
+
+void print_times_tables(int n)
+{
+	int num, x, y;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (x = 1; x <= n; x++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				y = num * x;
+				if (y <= 99)
+				_putchar(' ');
+				if (y <= 9)
+				_putchar(' ');
+				if (y >= 100)
+				{
+					_putchar((y / 100) + '0');
+					_putchar((y / 10) % 10 + '0');
+				}
+				else if (y <= 99 && y >= 10)
+				{
+					_putchar((y / 10) + '0');
+				}
+				_putchar((y % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
