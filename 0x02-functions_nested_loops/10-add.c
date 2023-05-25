770dd047d901402ba597c09442b192304d511ef1
Triumph Akin-James <triumphakinjames@gmail.com>
2023-05-25 02:45:40 +0100

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first number in the addition
+ * @n2: The second number in the addition
+ * Return: Result
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
